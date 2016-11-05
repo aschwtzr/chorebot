e91a65a6c1da6da39a582f2745953eedfd1a2dbd
aschwtzr <schweitzer.albert@gmail.com>
2016-11-05 18:34:40 -0400

    .

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mckibbinbot
 Test server for different Slackbots used in our McKibbin Loft
+
+Using forever: https://github.com/foreverjs/forever
+Using node-cron: https://github.com/kelektiv/node-cron
--- testserver.js
+++ testserver.js
@@ -1,15 +1,5 @@
 var http = require("http");
 
-// Import events module
-var events = require('events');
-
-// Create an eventEmitter object
-var eventEmitter = new events.EventEmitter();
-
-// Create an event handler as follows
-var connectHandler = function connected() {
-   console.log('connection succesful.');
-
 http.createServer(function (request, response) {
    // Send the HTTP header
    // HTTP Status: 200 : OK
@@ -19,30 +9,3 @@ http.createServer(function (request, response) {
    // Send the response body as "Hello World"
    response.end('Hello World\n');
 }).listen(8081);
-
-// Console will print the message
-console.log('Server running at http://104.131.33.162:8081/');
-
-
-
-
-
-
-
-
-   // Fire the data_received event
-   eventEmitter.emit('data_received');
-}
-
-// Bind the connection event with the handler
-eventEmitter.on('connection', connectHandler);
-
-// Bind the data_received event with the anonymous function
-eventEmitter.on('data_received', function(){
-   console.log('data received succesfully.');
-});
-
-// Fire the connection event
-eventEmitter.emit('connection');
-
-console.log("Program Ended.");
