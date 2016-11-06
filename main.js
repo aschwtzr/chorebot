6488da92c3df2b37edb3a9af0d680d0741d6d76f
aschwtzr <schweitzer.albert@gmail.com>
2016-11-06 13:10:22 -0500

    main for async reads

--- main.js
+++ main.js
@@ -1,27 +1,10 @@
-// Import events module
-var events = require('events');
+var fs = require("fs");
 
-
-// Create an eventEmitter object
-var eventEmitter = new events.EventEmitter();
-
-// Create an event handler as follows
-var connectHandler = function connected() {
-   console.log('connection succesful.');
-
-   // Fire the data_received event
-   eventEmitter.emit('data_received');
-}
-
-// Bind the connection event with the handler
-eventEmitter.on('connection', connectHandler);
-
-// Bind the data_received event with the anonymous function
-eventEmitter.on('data_received', function(){
-   console.log('data received succesfully.');
+// Asynchronous - Opening File
+console.log("Going to open file!");
+fs.open('input.txt', 'r+', function(err, fd) {
+   if (err) {
+      return console.error(err);
+   }
+  console.log("File opened successfully!");
 });
-
-// Fire the connection event
-eventEmitter.emit('connection');
-
-console.log("Program Ended.");
